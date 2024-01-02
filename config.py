a98b15f064562f483cb2dc46d73df85f6203fb02
a2g-flow <gliderinkflow@gmail.com>
2024-01-02 09:30:51 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -12,19 +12,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from @botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6987366378:AAHZ5_12fbiJLUDQ7tXV1QUnoUsupy7eQ2Q")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "22941360"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "cd8fb83bbf47a7e373b650bd338ad3d6")
 
     # Authorized users to use this bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 
     # session name
-    TG_USER_SESSION_NAME = os.environ.get("TG_USER_SESSION_NAME", "oxmohsen")
+    TG_USER_SESSION_NAME = os.environ.get("TG_USER_SESSION_NAME", "angrah")
 
     # tg user session string
     TG_USER_SESSION_STRING = os.environ.get("TG_USER_SESSION_STRING", "")
